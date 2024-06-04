4e6775cd2167dbfbaa6ea163a9fa1965c82b4360
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-06-04 16:39:02 +0200

    done

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 function convertToDays() {
     const age = document.getElementById('Age').value;
-    const days = (age * 365);
-    document.getElementById('result').innerHTML = "Your age is equal to    " + days + " days";
+    const days = (age * 365); //calculates the age to days
+    document.getElementById('result').innerHTML = "Your age is equal to    " + days + " days"; //this is the result
 }
